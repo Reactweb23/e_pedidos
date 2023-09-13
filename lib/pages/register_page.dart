0b87ef5d4c3ecbf3313641bb2390c52615f918ec
yraffic02 <yraffic02@gmail.com>
2023-09-12 22:06:27 -0300

    feat: refactore page register

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:e_pedidos_front/shared/widgets/custom_button.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -8,8 +10,157 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  bool isRegistered = false;
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(appBar: AppBar(),);
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.orange,
+        body: ConstrainedBox(
+          constraints: const BoxConstraints.expand(),
+          child: ListView(children: [
+            Container(
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                image: AssetImage('lib/assets/background.png'),
+                fit: BoxFit.cover,
+              )),
+              child: Column(children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.03,
+                ),
+                SvgPicture.asset("lib/assets/logo.svg"),
+                const Text("E-Pedidos"),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.7,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        color: Colors.grey[200], // Cor de fundo personalizada
+                      ),
+                      padding: const EdgeInsets.all(16.0),
+                      child: const Row(
+                        children: [
+                          Icon(Icons.verified_sharp, color: Colors.blue),
+                          SizedBox(
+                              width: 10.0), // Espaço entre o ícone e o texto
+                          Text(
+                            'Cadastro concluído',
+                            style: TextStyle(fontSize: 16.0),
+                          ),
+                        ],
+                      )),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.85,
+                  padding: const EdgeInsets.all(17),
+                  decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(17)),
+                      color: Colors.white),
+                  child: Column(
+                    children: [
+                      const Text('Preencha os dados para fazer o cadastro'),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const TextField(
+                        decoration: InputDecoration(
+                            hintText: 'Nome',
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)))),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const TextField(
+                        decoration: InputDecoration(
+                            hintText: 'Email',
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)))),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const TextField(
+                        decoration: InputDecoration(
+                            hintText: 'Cpf ou Cnpj',
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)))),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const TextField(
+                        decoration: InputDecoration(
+                            hintText: 'Endereço',
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)))),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const TextField(
+                        decoration: InputDecoration(
+                            hintText: 'Nome do estabelecimento',
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)))),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const TextField(
+                        decoration: InputDecoration(
+                            hintText: 'Categoria',
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)))),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const TextField(
+                        decoration: InputDecoration(
+                            hintText: 'Senha',
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)))),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const TextField(
+                        decoration: InputDecoration(
+                            hintText: 'Confirme a senha',
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)))),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      CustomButton(
+                        text: 'Cadastrar',
+                        backgroundColor: const Color.fromRGBO(54, 148, 178, 1),
+                        onPressed: () {},
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                )
+              ]),
+            ),
+          ]),
+        ),
+      ),
+    );
   }
 }
