1d6e3344148a0a40b45a779721cf7e8430cec4fe
yraffic02 <yraffic02@gmail.com>
2023-10-05 09:48:10 -0300

    feat: dialog add name category done

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: sized_box_for_whitespace
+
 import 'package:e_pedidos_front/shared/widgets/custom_button.dart';
 import 'package:e_pedidos_front/shared/widgets/custom_list_category.dart';
 import 'package:e_pedidos_front/shared/widgets/custom_layout.dart';
@@ -80,9 +82,61 @@ class _CategoryPageState extends State<CategoryPage> {
                         text: 'Adicionar categoria',
                         backgroundColor: const Color.fromRGBO(54, 148, 178, 1),
                         onPressed: () {
-                          showDialog(context: context, builder: (BuildContext bc){
-                                return Dialog();
-                          });
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext bc) {
+                                return Dialog(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10)),
+                                  backgroundColor: Colors.white,
+                                  child: Container(
+                                    height: 200,
+                                    width:
+                                        MediaQuery.of(context).size.width * 0.8,
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(horizontal: 31, vertical: 15),
+                                      child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                        children: [
+                                          const Text('Nome da Categoria', textAlign: TextAlign.start),
+                                          const TextField(
+                                            decoration: InputDecoration(
+                                              enabledBorder: OutlineInputBorder(
+                                                borderRadius: BorderRadius.all(
+                                                    Radius.circular(4)),
+                                                borderSide: BorderSide(
+                                                  color: Colors.blue,
+                                                  width: 2.0,
+                                                ),
+                                              ),
+                                              focusedBorder: OutlineInputBorder(
+                                                borderRadius: BorderRadius.all(
+                                                    Radius.circular(20)),
+                                                borderSide: BorderSide(
+                                                  color: Colors.blue,
+                                                  width: 2.0,
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            width: 178,
+                                            height: 50,
+                                            child: CustomButton(
+                                                text: 'Concluir',
+                                                textColor: const Color.fromRGBO(
+                                                    23, 160, 53, 1),
+                                                backgroundColor:
+                                                    const Color.fromRGBO(
+                                                        100, 255, 106, 1),
+                                                onPressed: () {}),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              });
                         }),
                 isRemove
                     ? Container()
@@ -97,18 +151,19 @@ class _CategoryPageState extends State<CategoryPage> {
                 isRemove == false
                     ? Container()
                     : SizedBox(
-                      height: 50,
-                      width: 178,
-                      child: CustomButton(
-                          text: 'Concluir',
-                          textColor: const Color.fromRGBO(23, 160, 53, 1),
-                          backgroundColor: const Color.fromRGBO(100, 255, 106, 1),
-                          onPressed: () {
-                            setState(() {
-                              isRemove = !isRemove;
-                            });
-                          }),
-                    )
+                        height: 50,
+                        width: 178,
+                        child: CustomButton(
+                            text: 'Concluir',
+                            textColor: const Color.fromRGBO(23, 160, 53, 1),
+                            backgroundColor:
+                                const Color.fromRGBO(100, 255, 106, 1),
+                            onPressed: () {
+                              setState(() {
+                                isRemove = !isRemove;
+                              });
+                            }),
+                      )
               ],
             ))
           ],
