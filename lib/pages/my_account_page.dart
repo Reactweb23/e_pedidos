2c170fb2565d223ff1dd1c918b17c3af8beb9bf0
yraffic02 <yraffic02@gmail.com>
2023-10-06 16:52:02 -0300

    feat: done page my acount

--- lib/pages/my_account_page.dart
+++ lib/pages/my_account_page.dart
@@ -1,4 +1,5 @@
 import 'package:e_pedidos_front/shared/widgets/custom_avatar.dart';
+import 'package:e_pedidos_front/shared/widgets/custom_button.dart';
 import 'package:e_pedidos_front/shared/widgets/custom_layout.dart';
 import 'package:flutter/material.dart';
 
@@ -12,70 +13,100 @@ class MyAccountPage extends StatefulWidget {
 class _MyAccountPageState extends State<MyAccountPage> {
   @override
   Widget build(BuildContext context) {
-    return const CustomLayout(
+    return CustomLayout(
         child: Scaffold(
       body: Padding(
-        padding: EdgeInsets.fromLTRB(31, 25, 31, 100),
+        padding: const EdgeInsets.fromLTRB(31, 25, 31, 100),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
+            const Text(
               'Minha conta',
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
             ),
-            Text('Adicione o novo produto do seu cardápio',
+            const Text('Adicione o novo produto do seu cardápio',
                 style: TextStyle(
                     fontSize: 13,
                     color: Color.fromRGBO(61, 61, 61, 1),
                     fontWeight: FontWeight.w500)),
-            SizedBox(
+            const SizedBox(
               height: 37,
             ),
-            Center(
-                child: Column(
-              children: [
-                CustomAvatar(),
-                SizedBox(
-                  height: 35,
-                ),
-                Row(children: [
-                    Text("Nome da empresa"),
-                    SizedBox(
-                      width: 48,
-                    ),
-                    Text("EcoVibe Bistrô")
-                ]),
-                Row(
-                  children: [
-                  Text("Responsável"),
-                  SizedBox(
-                    width: 48,
+            const Center(child: CustomAvatar()),
+            Expanded(
+              child: ListView(
+                children: [
+                  const SizedBox(
+                    height: 35,
                   ),
-                  Text("Paulo Farias")
-                ]),
-                Row(children: [
-                  Text("E-mail"),
-                  SizedBox(
-                    width: 48,
+                  const ListTile(
+                    leading: Text('Nome da empresa:'),
+                    title: Text('EcoVibe Bistrô'),
+                    contentPadding: EdgeInsets.all(0),
                   ),
-                  Text("ecovibebistro@gmail.com")
-                ]),
-                Row(children: [
-                  Text("Celular"),
-                  SizedBox(
-                    width: 48,
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    height: 1,
+                    decoration: const BoxDecoration(
+                        color: Color.fromRGBO(54, 148, 178, 1)),
                   ),
-                  Text("(81)99627-0938")
-                ]),
-                Row(children: [
-                  Text("CPF/CNPJ"),
-                  SizedBox(
-                    width: 48,
+                  const ListTile(
+                    leading: Text('Responsável:'),
+                    title: Text('Paulo Farias'),
+                    contentPadding: EdgeInsets.all(0),
                   ),
-                  Text("0000000000")
-                ]),
-              ],
-            ))
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    height: 1,
+                    decoration: const BoxDecoration(
+                        color: Color.fromRGBO(54, 148, 178, 1)),
+                  ),
+                  const ListTile(
+                    leading: Text('E-mail:'),
+                    title: Text('ecovibebistro@gmail.com'),
+                    contentPadding: EdgeInsets.all(0),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    height: 1,
+                    decoration: const BoxDecoration(
+                        color: Color.fromRGBO(54, 148, 178, 1)),
+                  ),
+                  const ListTile(
+                    leading: Text('Celular:'),
+                    title: Text('(81)99627-0938'),
+                    contentPadding: EdgeInsets.all(0),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    height: 1,
+                    decoration: const BoxDecoration(
+                        color: Color.fromRGBO(54, 148, 178, 1)),
+                  ),
+                  const ListTile(
+                    leading: Text('CPF/CNPJ:'),
+                    title: Text('0000000000'),
+                    contentPadding: EdgeInsets.all(0),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    height: 1,
+                    decoration: const BoxDecoration(
+                        color: Color.fromRGBO(54, 148, 178, 1)),
+                  ),
+                  const SizedBox(height: 35,),
+                  SizedBox(
+                    height: 50,
+                    width: 178,
+                    child: CustomButton(
+                        text: 'Editar',
+                        textColor: const Color.fromRGBO(150, 108, 0, 1),
+                        backgroundColor: const Color.fromRGBO(255, 223, 107, 1),
+                        onPressed: () {}),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
